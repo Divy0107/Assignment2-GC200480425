0bd768f1c79d3c3d0939c8bdfdf9f1412151ad58
Divy0107 <divy172002@gmail.com>
2022-07-23 16:56:30 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment2gc200480425 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment2gc200480425 to javafx.fxml;
+    exports com.example.assignment2gc200480425;
+}
